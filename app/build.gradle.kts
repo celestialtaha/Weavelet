9ff2987bb0fc690ee5ec4c27cf27a83eeb1f76ee
Taha <tahasamavati11@yahoo.com>
2026-03-21 05:56:11 +0100

    chore(release): bump version to 1.1.2
    Prepare patch release 1.1.2.
    
    - versionName: 1.1.0 -> 1.1.2
    - versionCode: 2 -> 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.wapp.wearmusic"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.2"
     }
 
     buildTypes {
