b0ac3a4b24c657cec9682b724d0c4bffad830d97
Taha <tahasamavati11@yahoo.com>
2026-06-25 10:12:40 +0200

    chore(release): prepare v1.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.wapp.wearmusic"
         minSdk = 29
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.3.0"
+        versionCode = 12
+        versionName = "1.3.5"
     }
 
     buildTypes {
